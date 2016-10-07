7b6f34f885c7ed53c9bfe325f0e81e829727bf23
Sabirat <rubya001@umn.edu>
2016-10-07 14:25:21 -0500

    first step

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.get('/', function(req, res){
+    res.sendFile( __dirname + '/' + 'index.html' );
+    })
+
+var port = process.env.PORT || 3000; // For when we deploy to Heroku
+app.listen(port)
